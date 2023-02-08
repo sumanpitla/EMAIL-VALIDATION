30db1a705a1328305779f9b38b4a092d768fb138
Suman_p <116899028+sumanpitla@users.noreply.github.com>
2023-02-08 20:04:13 +0530

    Add files via upload
    Check whether the given email is valid or not!

---
+++ email_validation.py
@@ -0,0 +1,13 @@
+import re
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+      print("Valid email")
+    else:
+      print("Invalid email")
+isValid("name.surname@gmail.com")
+isValid("anonymous123@yahoo.co.uk")
+isValid("anonymous123@...uk")
+isValid("...@domain.us")      
